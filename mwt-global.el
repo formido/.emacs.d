359ca22a4e1645ef6a74cc1db48be6b80fcabda9
Michael Terry <formido@gmail.com>
2008-11-30 22:05:07 -0800

    Save backups and auto-saves to /tmp updated .gitignore

--- mwt-global.el
+++ mwt-global.el
@@ -35,6 +35,11 @@
 (cond
  (platform-mac?
 
+  (autoload 'markdown-mode "lisp/markdown-mode.el"
+   "Major mode for editing Markdown files" t)
+  (setq auto-mode-alist
+   (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+
   ;; Set up scheme -------------------------------------------------------------
   (setq scheme-program-name "~/bin/plt/bin/mzscheme")
 
@@ -102,6 +107,29 @@
   
   ;; make script files executable
   (add-hook 'after-save-hook 'executable-make-buffer-file-executable-if-script-p)
+
+  ;; Put autosave files (ie #foo#) in one place, *not*
+  ;; scattered all over the file system!
+  (defvar autosave-dir
+    (concat "/tmp/emacs_autosaves/" (user-login-name) "/"))
+
+  (make-directory autosave-dir t)
+
+  (defun auto-save-file-name-p (filename)
+    (string-match "^#.*#$" (file-name-nondirectory filename)))
+
+  (defun make-auto-save-file-name ()
+    (concat autosave-dir
+	    (if buffer-file-name
+		(concat "#" (file-name-nondirectory buffer-file-name) "#")
+	      (expand-file-name
+	       (concat "#%" (buffer-name) "#")))))
+
+  ;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
+  ;; list contains regexp=>directory mappings; filenames matching a regexp are
+  ;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
+  (defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/"))
+  (setq backup-directory-alist (list (cons "." backup-dir)))
 
   ;; set up visual theme
   (require 'color-theme)
