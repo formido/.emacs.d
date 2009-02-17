122920846529ba08896fe892eb7caf89e7ffb3be
Michael Terry <Michael Terry@x2411MT.bruceclay.net>
2009-02-17 14:32:53 -0800

    Killed autosave and backup files. Added .jsm files to js2-mode.

--- mwt-global.el
+++ mwt-global.el
@@ -152,26 +152,35 @@
 
   ;; Put autosave files (ie #foo#) in one place, *not*
   ;; scattered all over the file system!
-  (defvar autosave-dir
-    (concat "/tmp/emacs_autosaves/" (user-login-name) "/"))
-
-  (make-directory autosave-dir t)
-
-  (defun auto-save-file-name-p (filename)
-    (string-match "^#.*#$" (file-name-nondirectory filename)))
-
-  (defun make-auto-save-file-name ()
-    (concat autosave-dir
-	    (if buffer-file-name
-		(concat "#" (file-name-nondirectory buffer-file-name) "#")
-	      (expand-file-name
-	       (concat "#%" (buffer-name) "#")))))
+  ;;   (defvar autosave-dir
+  ;;     (concat "/tmp/emacs_autosaves/" (user-login-name) "/"))
+  
+  ;;   (make-directory autosave-dir t)
+  
+  ;;   (defun auto-save-file-name-p (filename)
+  ;;     (string-match "^#.*#$" (file-name-nondirectory filename)))
+  
+  ;;   (defun make-auto-save-file-name ()
+  ;;     (concat autosave-dir
+  ;; 	    (if buffer-file-name
+  ;; 		(concat "#" (file-name-nondirectory buffer-file-name) "#")
+  ;; 	      (expand-file-name
+  ;; 	       (concat "#%" (buffer-name) "#")))))
 
   ;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
   ;; list contains regexp=>directory mappings; filenames matching a regexp are
   ;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
-  (defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/"))
-  (setq backup-directory-alist (list (cons "." backup-dir)))
+  ;; (defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/"))
+  ;; (setq backup-directory-alist (list (cons "." backup-dir)))
+
+  ;; Save all backup file in this directory.
+  ;; (setq backup-directory-alist (quote ((".*" . (expand-file-name "~/.emacs_backups/")))))
+
+  ;; Turn of backups.
+  (setq make-backup-files nil) 
+
+  ;; Turn off autosaves.
+  (setq auto-save-mode nil)
 
   ;; CEDET ---------------------------------------------------------------------
 
@@ -235,6 +244,7 @@
 ;;(autoload 'theirs/js2-mode "js2" nil t)
 (load "theirs/js2.elc")
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsm$" . js2-mode))
 
 ;; Load MozlRepl
 (autoload 'moz-minor-mode "moz" "Mozilla Minor and Inferior Mozilla Modes" t)
