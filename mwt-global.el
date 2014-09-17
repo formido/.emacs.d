7a28c849a931b82b905c00a299d06e0163c383c4
Michael Terry <michael@michaelterry.org>
2014-09-17 17:37:12 +0000

    fix typos

--- mwt-global.el
+++ mwt-global.el
@@ -8,7 +8,7 @@
       ;;(color-theme-twilight)
       ))
 
-;; Let's you undo and redo windo config with C-c left and C-c right
+;; Lets you undo and redo window config with C-c left and C-c right
 (when (fboundp 'winner-mode)
   (winner-mode 1))
 
