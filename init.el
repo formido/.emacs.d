23c2a308af7b10e883cafcaeda5c1fb4b61f4c67
Michael Terry <michael@michaelterry.org>
2014-09-18 23:00:23 +0000

    whereas I said add skewer-mode etc... ...now I mean it.

--- init.el
+++ init.el
@@ -9,11 +9,27 @@
   (require 'package-helper)
   (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (with-package simple-httpd)
   (with-package js2-mode*
 	      (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
-  (with-package (skewer-mode skewer-repl sgml-mode css-mode js2-mode)
-		(skewer-setup))
+  ;; (with-package (skewer-mode skewer-repl sgml-mode css-mode js2-mode)
+  ;; 		(skewer-setup))
   )
+
+(add-to-list 'load-path "~/.emacs.d/theirs/skewer-mode")
+
+(require 'skewer-mode)
+(autoload 'skewer-mode "skewer-mode")
+
+(require 'skewer-setup)
+(autoload 'skewer-setup "skewer-setup")
+(skewer-setup)
+
+(require 'skewer-repl)
+(autoload 'skewer-repl "skewer-repl")
+
+
+(require 'js2-mode)
 
 ;; Need some place to put elisp packages
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
