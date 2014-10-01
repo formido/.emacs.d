c3dedfc81867acef6b3ab861bb29b63e5a8dd9f9
Michael Terry <michael@michaelterry.org>
2014-10-01 20:59:56 +0000

    Automatically download missing packages from repos
    Fix to take advantage of package-helper's ability to pull packages
    from the official archive and melpa. Previously I'd had
    `package-initialize` in the wrong place, so some packages wouldn't
    be found.

--- init.el
+++ init.el
@@ -7,29 +7,15 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (require 'package-helper)
-  (package-initialize)
   (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (package-initialize)
+  (with-package magit*)
   (with-package simple-httpd)
   (with-package js2-mode*
 	      (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
-  ;; (with-package (skewer-mode skewer-repl sgml-mode css-mode js2-mode)
-  ;; 		(skewer-setup))
+  (with-package (skewer-mode* skewer-repl* sgml-mode* css-mode*)
+  		(skewer-setup))
   )
-
-(add-to-list 'load-path "~/.emacs.d/theirs/skewer-mode")
-
-(require 'skewer-mode)
-(autoload 'skewer-mode "skewer-mode")
-
-(require 'skewer-setup)
-(autoload 'skewer-setup "skewer-setup")
-(skewer-setup)
-
-(require 'skewer-repl)
-(autoload 'skewer-repl "skewer-repl")
-
-
-(require 'js2-mode)
 
 ;; Need some place to put elisp packages
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
