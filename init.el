9486a2443b25e3065f6a27d77de032ddd56ea953
Michael Terry <michael@michaelterry.org>
2015-02-12 23:16:49 +0000

    Add web-mode

--- init.el
+++ init.el
@@ -12,8 +12,18 @@
   (with-package magit*)
   (with-package simple-httpd)
   (with-package gist)
+  (with-package web-mode*
+    (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+    (defun web-mode-hooks ()
+      "Hooks for web-mode."
+      (setq web-mode-markup-indent-offset 2)
+      (setq web-mode-css-indent-offset 2)
+      (setq web-mode-code-indent-offset 2)
+      (setq web-mode-enable-engine-detection t)
+      )
+    (add-hook 'web-mode-hook 'web-mode-hooks))
   (with-package js2-mode*
-	      (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
+    (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode)))
   (with-package (skewer-mode* skewer-repl* sgml-mode* css-mode*)
   		(skewer-setup))
   )
@@ -25,7 +35,6 @@
 ;; personal customizations
 (require 'mwt-global)
 
-(put 'dired-find-alternate-file 'disabled nil)
 
 ;; Ahhh, yes, now I can work like I used to in 
 ;; Satimage's Smile :) (No pun intended)
@@ -37,3 +46,23 @@
 (add-hook 'sql-interactive-mode-hook
 	  (lambda ()
 	    (toggle-truncate-lines t)))
+(put 'dired-find-alternate-file 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mouse-wheel-mode t nil (mwheel))
+ '(quack-default-program "~/bin/plt/bin/mzscheme")
+ '(quack-programs (quote ("~/bin/plt/bin/mzscheme" "bigloo" "csi" "csi -hygienic" "gosh" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "mred -z" "mzscheme" "mzscheme -M
+    errortrace" "mzscheme3m" "mzschemecgc" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi")))
+ '(quack-remember-new-programs-p t)
+ '(regex-tool-backend (quote perl))
+ '(safe-local-variable-values (quote ((engine . django))))
+ '(transient-mark-mode (quote identity)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
