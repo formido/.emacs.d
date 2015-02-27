a9fea4a5c4b3f3d6da43664a42462c055a00121e
Michael Terry <michael@michaelterry.org>
2015-02-27 23:27:42 +0000

    fix gist install

--- init.el
+++ init.el
@@ -28,6 +28,107 @@
   		(skewer-setup))
   )
 
+;; Set sql-mode tab width to 2
+(add-hook 'text-mode-hook (lambda ()
+  (setq indent-tabs-mode t)
+  (setq tab-stop-list (number-sequence 2 200 2))
+  (setq tab-width 2)
+  (setq indent-line-function 'insert-tab) ))
+
+(add-to-list 'auto-mode-alist '("\\.sql$" . sql-mode))
+
+(add-to-list 'auto-mode-alist
+             '("\\.psql$" . (lambda ()
+                              (sql-mode)
+                              (sql-highlight-postgres-keywords))))
+
+;;
+;; Fix SQL indentation
+;;
+
+(defun get-previous-indentation ()
+  "Get the column of the previous indented line"
+  (interactive)
+  (save-excursion
+    (progn
+      (move-beginning-of-line nil)
+      (skip-chars-backward "\n \t")
+      (back-to-indentation))
+    (current-column)))
+
+(defun get-current-indentation ()
+  "Return column at current indentation"
+  (interactive)
+  (save-excursion
+    (progn
+      (back-to-indentation)
+      (current-column))))
+
+(defun point-at-current-indentation ()
+  "Return point at current indentation"  
+  (interactive)
+  (save-excursion
+    (progn
+      (move-to-column (get-current-indentation))
+      (point))))
+
+(defun point-at-column-on-line (col)
+  "Returns the point at `col` on the current line"
+  (interactive)
+  (save-excursion
+    (progn
+      (move-to-column col)
+      (point))))
+
+(defun ig-move-line-to-column (col)
+  "Move the line to col; fill with all spaces if moveing forward"
+  (interactive "p")
+  (let ((point-at-cur-indent (point-at-current-indentation))
+        (col-at-cur-indent (get-current-indentation)))
+    (cond (
+           (= col 0)
+           ;; delete to beginning of line or do nothing
+           (if (= col-at-cur-indent 0)
+               nil
+             (delete-region point-at-cur-indent (point-at-column-on-line 0))))
+          (
+           (< col col-at-cur-indent)
+           ;; delete from our current point BACK to col
+           (delete-region (point-at-column-on-line col) point-at-cur-indent))
+          (
+           (> col col-at-cur-indent)
+           ;; delete all text from indent to beginning of line
+           (progn
+             (delete-region point-at-cur-indent (point-at-column-on-line 0))
+             (move-beginning-of-line nil)
+             ;; add spaces forward
+             (insert (make-string col ?\s)))))))
+
+(defun ig-indent-sql ()
+  "Indent by `tab-width` at most 1 time greater than the previously indented line otherwise go to the beginning of the line indent forward by `tab-width`"
+  (let ((previous (get-previous-indentation))
+        (current (get-current-indentation)))
+    (cond ( ;; exactly at previous line's indentation
+           (= previous current)
+           (ig-move-line-to-column (+ current tab-width)))
+
+          ( ;; current is greater than previous
+           (> current previous)
+           ;; exactly at one indentation forward from previous lines indent
+           (if (= tab-width (- current previous))
+               ;; move line to beginning
+               (ig-move-line-to-column 0)
+             ;; go back to previous indentation level
+             (ig-move-line-to-column previous)))
+
+          (t 
+           (ig-move-line-to-column (+ current tab-width))))))
+
+(add-hook 'sql-mode-hook
+          (function (lambda ()
+                      (make-local-variable 'indent-line-function)
+                      (setq indent-line-function 'ig-indent-sql))))
+
 ;; Need some place to put elisp packages
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/theirs"))
